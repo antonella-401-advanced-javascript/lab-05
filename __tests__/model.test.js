1e9b0f104a436f4d561706e996dad061505400b0
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-09-24 16:41:49 -0700

    done with test

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -35,6 +35,48 @@ describe('Dog model', () => {
     expect(errors.breed.kind).toBe('required');
     expect(errors['appearance.color'].kind).toBe('required');
     expect(errors.weight.kind).toBe('required');
-  })
+  });
+
+  it('populates default properties', () => {
+    const dogData = {
+      breed: 'Alaskan Klee Kai',
+      nicknames: ['AKK', 'Klee Kai', 'Miniature Alaskan Husky', 'Mini Husky'],
+      size: ['small', 'medium'],
+      appearance: {
+        pattern: 'bicolor',
+        color: 'grey and white'
+      },
+      weight: 16
+    };
+
+    const dog = new Dog(dogData);
+    const err = dog.validateSync();
+    expect(err).toBeUndefined();
+
+    expect(dog.purebred).toBe(true);
+  });
+
+  it('enforces min', () => {
+    const dogData = {
+      weight: 0
+    };
+    const dog = new Dog(dogData);
+
+    const { errors } = dog.validateSync();
+
+    expect(errors.weight.kind).toBe('min');
+  });
+
+  it('enforces max', () => {
+    const dogData = {
+      weight: 121
+    };
+    const dog = new Dog(dogData);
+
+    const { errors } = dog.validateSync();
+
+    expect(errors.weight.kind).toBe('max');
+
+  });
   
 });
